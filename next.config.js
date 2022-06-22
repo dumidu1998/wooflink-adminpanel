27b1fae3ec65404cc472f2bbcb4c089520025452
dumidu1998 <dumiduraj@gmail.com>
2022-06-23 01:21:19 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
   experimental: {
+    outputStandalone: true,
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
